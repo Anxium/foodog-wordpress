5d41b04135ac1904c1419d9d2c7035f7f6372e71
Sophie Favier <sophie.fav05@gmail.com>
2019-01-04 16:59:44 +0100

    header update

--- wp-content/themes/RASH/header.php
+++ wp-content/themes/RASH/header.php
@@ -7,22 +7,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title><?php bloginfo('name'); ?></title>
+    <title>Exemple de thème WordPress</title>
     <!– Ajout d’une nouvelle feuille de style qui sera spécifique à notre thème –>
     <link href="<?php bloginfo('template_directory');?>/style.css" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
 <body>
-    <div class="header">
-        <div class="container">
-            <nav id="navigation-principale" role="navigation"> 
-                <?php wp_nav_menu( array('theme_location' => 'main-menu') ); ?>
-            </nav>
+    <div class="blog-header">
+        <div class="container-fluid">
+            <div class="row header-link">
+                <div class="col-sm-11 title">
+                    <h1 class="blog-title"><?= get_bloginfo('name'); ?></h1>
+                </div>
+                <div class="col-sm-1 nav-social">
+                    <?php wp_nav_menu( array('theme_location' => 'social-media') ); ?>
+                </div>
+            </div>
         </div>
     </div>
+    <div class="header">
+        <div class="container">
+            <nav id="navigation-principale" role="navigation" class="menu"> 
+                <?php wp_nav_menu( array('theme_location' => 'main-menu') ); ?>
+            </nav>
+        </div> 
+    </div>
     <div class="container">
-        <div class="blog-header">
-            <h1 class="blog-title"><?= get_bloginfo('name'); ?></h1>
-            <p class="lead blog-description"><?= get_bloginfo('description'); ?></p>
-        </div>
+
