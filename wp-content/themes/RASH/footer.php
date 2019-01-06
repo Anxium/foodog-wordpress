79d969cfe6fd323e623f6e9b638b5382f3859888
Hadrien Soumoy <hadriensoumoy@gmail.com>
2019-01-06 20:04:36 +0100

    push01

--- wp-content/themes/RASH/footer.php
+++ wp-content/themes/RASH/footer.php
@@ -1,6 +1,136 @@
     </div>
 
     <footer class="blog-footer">
+
+    <div class="footer">
+        <div class="footer1">
+            <div class="container-fluid">
+
+                <div class="container ">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h2>Categories</h2>
+                            <p>
+                                <div class="myframegmap">
+
+                                 <i class="fa fa-home"></i>    <a href="#"> Home</a><br>
+                                <i class="fa fa-user-o"></i>    <a href="#"> About Us</a><br>
+                                <i class="fa fa-map-marker"></i>    <a href="#"> Contact Us</a><br>
+                                <i class="fa fa-briefcase"></i>    <a href="#"> Services</a><br>
+                                <i class="fa fa-question-circle"></i>    <a href="#"> Term & Conditions</a><br><br>
+
+                                </div>
+                            </p><br>
+                        </div>
+                        <div class="col-sm-4">
+                            <h2>Popular Posts</h2>
+                            <p>
+                                <i class="fa fa-home"></i>    <a href="#"> Home</a><br>
+                                <i class="fa fa-user-o"></i>    <a href="#"> About Us</a><br>
+                                <i class="fa fa-map-marker"></i>    <a href="#"> Contact Us</a><br>
+                                <i class="fa fa-briefcase"></i>    <a href="#"> Services</a><br>
+                                <i class="fa fa-question-circle"></i>    <a href="#"> Term & Conditions</a><br><br>
+
+
+                            </p>
+
+
+                        </div>
+                        <div class="col-sm-4 ">
+                            <h2>Instagram</h2>
+                           <div class="row gallery-bottom">
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="row ">
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                 <div class="gallery-image">
+                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
+                                 </div>
+                              </div>
+                              
+                              
+                              
+
+                            <br>
+                        </div>
+                    </div>
+                    <div class="clear30"></div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="footer2">
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="row">
+                        <div class="clear30"></div>
+                        <div class="col-sm-12 text-center">
+                            <p><strong>copyright © 2013-2018 All right reserved.</strong></p>
+                        </div>
+                        <div class="clear30"></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
         <p>Par <a href="https://www.wppourlesnuls.com/">WP Pour les Nuls</a>.</p>
     </footer>
 
