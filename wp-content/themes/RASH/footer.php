ee73a22dc8d67075d4184ce83ba417f775df43fb
Hadrien Soumoy <hadriensoumoy@gmail.com>
2019-01-15 09:31:25 +0100

    footer termine second push

--- wp-content/themes/RASH/footer.php
+++ wp-content/themes/RASH/footer.php
@@ -1,143 +1,100 @@
-    </div>
+</div>
 
-    <footer class="blog-footer">
+<footer class="footer1">
+   <div class="container">
+      <div class="row gallery-bottom">
+         <div class="col-sm-4">
 
-    <div class="footer">
-        <div class="footer1">
-            <div class="container-fluid">
-
-                <div class="container ">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <h2>Categories</h2>
-                            <p>
-                                <div class="myframegmap">
-
-                                 <i class="fa fa-home"></i>    <a href="#"> Home</a><br>
-                                <i class="fa fa-user-o"></i>    <a href="#"> About Us</a><br>
-                                <i class="fa fa-map-marker"></i>    <a href="#"> Contact Us</a><br>
-                                <i class="fa fa-briefcase"></i>    <a href="#"> Services</a><br>
-                                <i class="fa fa-question-circle"></i>    <a href="#"> Term & Conditions</a><br><br>
-
-                                </div>
-                            </p><br>
-                        </div>
-                        <div class="col-sm-4">
-                            <h2>Popular Posts</h2>
-                            <p>
-                                <i class="fa fa-home"></i>    <a href="#"> Home</a><br>
-                                <i class="fa fa-user-o"></i>    <a href="#"> About Us</a><br>
-                                <i class="fa fa-map-marker"></i>    <a href="#"> Contact Us</a><br>
-                                <i class="fa fa-briefcase"></i>    <a href="#"> Services</a><br>
-                                <i class="fa fa-question-circle"></i>    <a href="#"> Term & Conditions</a><br><br>
-
-
-                            </p>
-
-
-                        </div>
-                        <div class="col-sm-4 ">
-                            <h2>Instagram</h2>
-                           <div class="row gallery-bottom">
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                           </div>
-                           <div class="row ">
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                   <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                 <div class="gallery-image">
-                                    <img src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="image view">
-                                 </div>
-                              </div>
-                              
-                              
-                              
-
-                            <br>
-                        </div>
-                    </div>
-                    <div class="clear30"></div>
-                </div>
+            <a href="#" class="category_footer">Categories</a>
+            <div class="footer-link">
+               <?php wp_nav_menu( array('theme_location' => 'footer-menu') ); ?>
             </div>
-        </div>
+         </div>
+         <div class="col-sm-4">
+            <a href="#" class="category_footer">Popular Posts</a>
+            <div>
 
+               <?php /*
+                     if( is_active_sidebar( 'zone-widgets-2' ) ):
+                     dynamic_sidebar( 'zone-widgets-2' );
+                     endif;
+            */   ?>
 
-        <div class="footer2">
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="clear30"></div>
-                        <div class="col-sm-12 text-center">
-                            <p><strong>copyright © 2013-2018 All right reserved.</strong></p>
-                        </div>
-                        <div class="clear30"></div>
-                    </div>
+               <?php
+$popular_posts_args = array(
+ 'posts_per_page' => 3,
+ 'meta_key' => 'my_post_viewed',
+ 'orderby' => 'meta_value_num',
+ 'order'=> 'DESC'
+);
+ 
+$popular_posts_loop = new WP_Query( $popular_posts_args );
+ 
 
-                </div>
+?>
 
+               <?php  while( $popular_posts_loop->have_posts() ): $popular_posts_loop->the_post(); ?>
+               <div class="footer_div">
+                  <?php if (has_post_thumbnail()) : ?>
+                  <a class="hadri-img" href="" title=»<?php the_title_attribute(); ?> » >
+                     <?php the_post_thumbnail(); ?></a>
+                  <h3 class="title2"><a class="block" href="" title=»<?php the_title_attribute(); ?> »>
+                        <?php the_title(); ?></a></h3>
+                  <?php endif ?>
+               </div>
+               <?php  endwhile;
+wp_reset_query(); ?>
+               <!--
+               <a href="</*?php the_permalink() ?>"> - <?php the_title(); ?></a>
+                  <img class="block " src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster"><span class="lorem">  Evenlet Ut Molesti</span><br>
+                  <img class="block " src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster"><span class="lorem">  Earum Rerum Tenet</span><br>
+                  <img class="block " src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster"><span class="lorem">  Evenlet Ut Molesti</span><br>
+                  <img class="block " src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster"><span class="lorem">  Earum Rerum Tenet</span><br>
+                      -->
             </div>
-        </div>
-    </div>
+         </div>
+         <div class="col-sm-4 ">
+            <a href="#" class="category_footer">Instagram</a>
+            <!-- InstaWidget -->
+            <a href="https://instawidget.net/v/tag/cutedogs" id="link-52194829e90539bb02c64185c02ae949fc0be36409a0eae50269b1cf440a4b45">#cutedogs</a>
+            <script src="https://instawidget.net/js/instawidget.js?u=52194829e90539bb02c64185c02ae949fc0be36409a0eae50269b1cf440a4b45&width=300px"></script>
+            <!--
+                    <div class="row">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                    </div>
+                  <div class="row">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                    </div>
+                  <div class="row">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+                        <img class="block" src="https://igx.4sqi.net/img/general/200x200/0_OGr2UYgM7kwo1CB-AMVaDzY2rtFT3Ae7jf21-FEvY.jpg" alt="poster">
+   -->
+         </div>
+      </div>
+   </div>
+   
+   <div class="footer2">
+      <div class="container-fluid footer-copy">
+         <div class="row">
+            <div class="col-sm-12 text-center">
+               <p><strong>copyright © 2013-2018 All right reserved.</strong></p>
+            </div>
+         </div>
+      </div>
+   </div>
 
-        <p>Par <a href="https://www.wppourlesnuls.com/">WP Pour les Nuls</a>.</p>
-    </footer>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</footer>
 
-    <?php wp_footer(); ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<?php wp_footer(); ?>
 
 </body>
 
--- wp-content/themes/RASH/functions.php
+++ wp-content/themes/RASH/functions.php
@@ -3,6 +3,8 @@
 register_nav_menus( array(
     'main-menu' => 'Menu principal',
     'second-menu' => 'Second menu',
+    'footer-menu' => 'menu footer',
+    'footer-middle' => 'middle fotter'
 ));
 
 function wpdocs_theme_name_scripts()
@@ -13,3 +15,46 @@ function wpdocs_theme_name_scripts()
     wp_enqueue_style('bootstrap-style');
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
+
+if ( function_exists( 'add_theme_support' ) ) {
+    add_theme_support( 'post-thumbnails' );
+  }
+
+function new_excerpt_more( $more ) {
+	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
+
+$the_query = new WP_Query('cat=1&showposts=3&orderby=rand');
+
+
+function notux_widgets_init() {	
+	// Mon widget sur mesure
+		register_sidebar( array(
+			'name'			=> __( 'Nom de ma zone de widgets', 'dossierdevotretheme' ),
+			'id'			=> 'zone-widgets-1',
+			'description'	=> __( 'Description de la zone de widgets.', 'dossierdevotretheme' ),
+			'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
+			'after_widget'	=> '</div>',
+			'before_title'	=> '<div class="widget-title th3">',
+			'after_title'	=> '</div>',
+		) );
+}
+add_action( 'widgets_init', 'notux_widgets_init' );
+
+
+/*lastest post*/
+
+function count_post_visits() {
+	if( is_single() ) {
+	global $post;
+	$views = get_post_meta( $post->ID, 'my_post_viewed', true );
+	if( $views == '' ) {
+	update_post_meta( $post->ID, 'my_post_viewed', '1' ); 
+	} else {
+	$views_no = intval( $views );
+	update_post_meta( $post->ID, 'my_post_viewed', ++$views_no );
+	}
+	}
+   }
+   add_action( 'wp_head', 'count_post_visits' );
