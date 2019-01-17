6a2018466e0d7037fcf924a83c3c1a2a21a4ae9e
rubenpereirac <rubenpereira.work@gmail.com>
2019-01-17 14:19:30 +0100

    ajout de la sidebar

--- wp-content/themes/RASH/functions.php
+++ wp-content/themes/RASH/functions.php
@@ -7,9 +7,13 @@ register_nav_menus( array(
 
 function wpdocs_theme_name_scripts()
 {
-    wp_register_style('main-style', get_template_directory_uri().'/blog.css', array(), true);
-    wp_enqueue_style('main-style');
     wp_register_style('bootstrap-style', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css', array(), true);
     wp_enqueue_style('bootstrap-style');
+    wp_register_style('main-style', get_template_directory_uri().'/style.css', array(), true);
+    wp_enqueue_style('main-style');
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
+
+// Ajoute les thumbnails
+add_theme_support('post-thumbnails');
+add_image_size( 'single-post-thumbnail', 356, 250);
